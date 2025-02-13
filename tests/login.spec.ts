4bce7b59c1b6a95929ba237c1cdb72c031bf9386
Hudson Stohl <hrstohl02@gmail.com>
2025-02-12 22:25:31 -0700

    added diner dashboard test

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -75,3 +75,48 @@ test("login", async ({ page }) => {
   await page.getByRole("link", { name: "Logout" }).click();
   await expect(page.getByRole("link", { name: "Register" })).toBeVisible();
 });
+
+test("diner dashboard", async ({ page }) => {
+  await page.route("*/**/api/auth", async (route) => {
+    if (route.request().method() == "PUT") {
+      const loginReq = { email: "d@jwt.com", password: "a" };
+      const loginRes = {
+        user: {
+          id: 3,
+          name: "Kai Chen",
+          email: "d@jwt.com",
+          roles: [{ role: "diner" }],
+        },
+        token: "abcdef",
+      };
+      expect(route.request().method()).toBe("PUT");
+      expect(route.request().postDataJSON()).toMatchObject(loginReq);
+      await route.fulfill({ json: loginRes });
+    } else if (route.request().method() == "DELETE") {
+      //expect(route.request().headers()["Authorization"]).toBe("Bearer abcdef");
+      const logoutRes = { message: "logout successful" };
+      await route.fulfill({ json: logoutRes });
+    } else {
+      fail("unexpected request");
+    }
+  });
+
+  await page.goto("/");
+
+  await page.getByRole("link", { name: "Login" }).click();
+  await page.getByRole("textbox", { name: "Email address" }).click();
+  await page.getByRole("textbox", { name: "Email address" }).fill("d@jwt.com");
+  await page.getByRole("textbox", { name: "Password" }).click();
+  await page.getByRole("textbox", { name: "Password" }).fill("a");
+  await page.getByRole("button", { name: "Login" }).click();
+  await expect(page.getByRole("link", { name: "Register" })).not.toBeVisible();
+  await expect(page.locator("#navbar-dark")).toContainText("Logout");
+  await expect(page.getByLabel("Global")).toContainText("KC");
+
+  await page.getByRole("link", { name: "KC" }).click();
+  await page.getByText("Your pizza kitchen").click();
+  await page.getByText("Kai Chen").click();
+  await page.getByText("diner", { exact: true }).click();
+  await page.getByRole("link", { name: "Buy one" }).click();
+  await page.getByText("Awesome is a click away").click();
+});
